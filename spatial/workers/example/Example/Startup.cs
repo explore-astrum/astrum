a422716b6c5883eec62fc77364266c2f5a030380
Dax Ravi <d@ironbay.co>
2018-11-18 21:58:19 -0500

    Progress

--- spatial/workers/example/Example/Startup.cs
+++ spatial/workers/example/Example/Startup.cs
@@ -15,31 +15,13 @@ namespace Example
             var loop = new Example.Loop();
             loop.Execute((dispatcher, connection) =>
             {
-                var entityIdReservationRequestId = connection.SendReserveEntityIdRequest(100);
-                Console.WriteLine(entityIdReservationRequestId);
-                var entityCreationRequestId = default(RequestId<CreateEntityRequest>);
-
-                dispatcher.OnReserveEntityIdResponse(op =>
-                {
-                    Console.WriteLine(op.StatusCode);
-                    if (op.RequestId == entityIdReservationRequestId && op.StatusCode == StatusCode.Success)
-                    {
-                        Console.WriteLine("cool");
-                        var entity = new Entity();
-                        // Empty ACL - should be customised.
-                        entity.Add(new Improbable.EntityAcl.Data(
-                          new Improbable.WorkerRequirementSet(new Improbable.Collections.List<Improbable.WorkerAttributeSet>()),
-                          new Improbable.Collections.Map<uint, Improbable.WorkerRequirementSet>()));
-                        // Needed for the entity to be persisted in snapshots.
-                        entity.Add(new Improbable.Persistence.Data());
-                        entity.Add(new Improbable.Metadata.Data("test"));
-                        entity.Add(new Improbable.Position.Data(new Improbable.Coordinates(1, 2, 3)));
-                        entityCreationRequestId = connection.SendCreateEntityRequest(entity, op.EntityId, 100);
-                    }
-                });
+                var entity = Astrum.Item.Manager.Create(0);
+                connection.SendCreateEntityRequest(entity, null, 100);
 
                 dispatcher.OnCreateEntityResponse(op =>
                 {
+                    Console.WriteLine(op.EntityId);
+                    Console.WriteLine(op.StatusCode);
                     Console.WriteLine("Entity created");
                 });
 
