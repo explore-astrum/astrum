0bc2510d29390539c87bd868639d72bb42d7f0d3
Dax Ravi <d@ironbay.co>
2019-06-30 16:34:20 -0400

    Rework theme

--- web2/src/pages/landing-page/index.tsx
+++ web2/src/pages/landing-page/index.tsx
@@ -52,10 +52,11 @@ export default class LandingPage extends React.Component<any, any> {
             // require('./img/layer-0.png'),
         ]
         const FRONT = require('./img/layer-0.png')
-        const loaded =
-            LAYERS.filter((_item, index) => this.state[index] != true).length == 0
+        // const loaded =
+        // LAYERS.filter((_item, index) => this.state[index] != true).length == 0
+        const loaded = true
         return (
-            <Container bg-black style={{ opacity: loaded ? 100 : 0 }}>
+            <Container fg-white bg-black style={{ opacity: loaded ? 100 : 0 }}>
                 {
                     LAYERS.map((item, index) => (
                         <Container key={item} fill overflow-hidden style={{
@@ -73,20 +74,19 @@ export default class LandingPage extends React.Component<any, any> {
                 <Container>
                     <Image src={FRONT} relative />
                 </Container>
-                <Container relative style={{ zIndex: 1000, background: '#212529' }}>
+                <Container relative style={{ zIndex: 1000, background: '#000000' }}>
                     <Wrap>
                         <Container >
                             <Container
                                 pad-v8
                                 m-mgn-v8
-                                style={{ fontFamily: 'Thruster', letterSpacing: '4px' }}
-                                uppercase
-                                fg-white
-                                size-6
-                                s-size-8
-                                m-size-10
-                                text-center>
-                                Welcome to Astrum
+                                fg-white>
+                                <Container text-center size-6>
+                                    A new world to conquer
+                                </Container>
+                                <Container text-center size-10 weight-8 mgn-t4>
+                                    Welcome to Astrum
+                                </Container>
                             </Container>
                             <Container relative cursor-pointer>
                                 <video
@@ -107,79 +107,119 @@ export default class LandingPage extends React.Component<any, any> {
                                     </Container>
                                 }
                             </Container>
-                            <Container flex flex-column align-center pad-v8 mgn-h6>
-                                <Container fg-white size-4 m-size-5 weight-5 line-8 text-center>
-                                    Astrum is an experiment in building a singular world owned and operated entirely by its players. Claim your piece to engage in 100% player driven trade, exploration, politics and work together to determine this future of this new world
+                            <Container
+                                mgn-t8
+                                flex
+                                fg-white
+                                size-5
+                                weight-5
+                                line-8
+                                text-center>
+                                <Container flex-5 pad-r4>
+                                    Astrum is an experiment in building a singular world owned and operated entirely by you and your fellow inhabitants
+                                </Container>
+                                <Container flex-5 pad-l4>
+                                    Claim a piece to engage in 100% player driven trade, exploration, politics and work together to determine this future of this new world
                                 </Container>
                             </Container>
                         </Container>
-                        <Container pad-b8>
-                            <Terrain />
-                            <Container flex justify-center mgn-t4>
-                                <Container mgn-t4 fg-black bg-white pad-h6 pad-v4 uppercase radius-4 weight-6 size-3-5>Browse Available Plots</Container>
-                            </Container>
+                        <Container flex justify-center pad-t8>
+                            <Container
+                                fg-black
+                                bg-yellow
+                                pad-h6
+                                pad-v6
+                                cursor-pointer
+                                uppercase
+                                weight-8
+                            >Browse Available Plots →</Container>
                         </Container>
-                        <Container mgn-v8 fg-white>
-                            <Container flex justify-center>
-                                <Container flex-5 relative pad-r4>
+                        <Container fg-white mgn-t8>
+                            <Container flex align-center pad-t4>
+                                <Container flex-5 pad-r4>
+                                    <Container flex align-center>
+                                        <Container
+                                            style={{ fontSize: '6rem' }}
+                                            weight-6>01</Container>
+                                        {/* <Container size-5 pad-l4 weight-5>Claim your piece</Container> */}
+                                    </Container>
+                                    <Container weight-6 mgn-t8 size-5 >
+                                        Claim your piece
+                                    </Container>
+                                    <Container line-8 mgn-t8>
+                                        <Container>
+                                            Astrum is a single, persistent landscape that the players completely own. Every rock and tree is hand-crafted by our artists to create a bespoke, unique property for every land owner.
+                                        </Container>
+                                        <Container mgn-t8>
+                                            After purchasing a plot of land, you and only you are able to build on top of it. No one will be able to claim any part of your land until you sell or trade it yourself.
+                                        </Container>
+                                    </Container>
+                                </Container>
+                                <Container flex-5 pad-l4>
                                     <Image src={IMG_STEP_2} />
                                 </Container>
-                                <Container flex-5 flex align-center pad-l4>
-                                    <Container>
-                                        <Container uppercase size-5 weight-6>Claim your piece</Container>
-                                        <Container mgn-t2 line-8 >
-                                            Astrum is a single, persistent landscape that the players completely own. Every rock and tree is hand-crafted by our artists to create a bespoke, unique property for every land owner. After purchasing a plot of land, you and only you are able to build on top of it. No one will be able to claim any part of your land until you sell or trade it yourself.
-                                    </Container>
-                                    </Container>
-                                </Container>
                             </Container>
-                            <Container flex justify-center mgn-t8>
-                                <Container flex-5 flex align-center pad-r4>
-                                    <Container>
-                                        <Container uppercase size-5 weight-6>Make it yours</Container>
-                                        <Container mgn-t2 line-8 >
-                                            Every relic found in-game and on someone’s property has been bought, traded, or sold on the marketplace. Every item is scarce, so there’s a limit to the amount of houses, vehicles, artwork, etc. You can travel to an water-side property to admire their dock and barter with the owner to improve what’s in your inventory and on your land.
-                                    </Container>
-                                    </Container>
-                                </Container>
-                                <Container flex-5 relative pad-l4>
+                            <Container flex align-center mgn-t8 pad-t8>
+                                <Container flex-5 pad-r4>
                                     <Image src={IMG_STEP_1} />
                                 </Container>
-                            </Container>
-                            <Container flex justify-center mgn-t8>
-                                <Container flex-5 relative pad-r4>
-                                    <Image src={IMG_STEP_0} />
+                                <Container flex-5 pad-l4>
+                                    <Container flex align-center>
+                                        <Container
+                                            style={{ fontSize: '6rem' }}
+                                            weight-6>02</Container>
+                                        {/* <Container size-5 pad-l4 weight-5>Claim your piece</Container> */}
+                                    </Container>
+                                    <Container weight-6 mgn-t8 size-5>
+                                        Make it your own
+                                    </Container>
+                                    <Container line-8 mgn-t8>
+                                        <Container>
+                                            Astrum is a single, persistent landscape that the players completely own. Every rock and tree is hand-crafted by our artists to create a bespoke, unique property for every land owner.
+                                        </Container>
+                                        <Container mgn-t8>
+                                            After purchasing a plot of land, you and only you are able to build on top of it. No one will be able to claim any part of your land until you sell or trade it yourself.
+                                        </Container>
+                                    </Container>
                                 </Container>
-                                <Container flex-5 flex align-center pad-l4>
-                                    <Container>
-                                        <Container uppercase size-5 weight-6>Explore and Trade</Container>
-                                        <Container mgn-t2 line-8 >
-                                            Any relics (assets) that you own can be placed on your land or combined with another relic. Make the castle or starship port of your dreams. As a creator, you can upload your own relics, set an amount and price, and release them on the marketplace.
+                            </Container>
+                            <Container flex align-center pad-t8 mgn-t8>
+                                <Container flex-5 pad-r4>
+                                    <Container flex align-center>
+                                        <Container
+                                            style={{ fontSize: '6rem' }}
+                                            weight-6>03</Container>
+                                        {/* <Container size-5 pad-l4 weight-5>Claim your piece</Container> */}
                                     </Container>
+                                    <Container weight-6 mgn-t8 size-5>
+                                        Explore and trade
                                     </Container>
+                                    <Container line-8 mgn-t8>
+                                        <Container>
+                                            Astrum is a single, persistent landscape that the players completely own. Every rock and tree is hand-crafted by our artists to create a bespoke, unique property for every land owner.
+                                        </Container>
+                                        <Container mgn-t8>
+                                            After purchasing a plot of land, you and only you are able to build on top of it. No one will be able to claim any part of your land until you sell or trade it yourself.
+                                        </Container>
+                                    </Container>
+                                </Container>
+                                <Container flex-5 pad-l4>
+                                    <Image src={IMG_STEP_0} />
                                 </Container>
                             </Container>
                         </Container>
-                        <Container fg-white>
-                            <Container
-                                style={{ fontFamily: 'Thruster', letterSpacing: '4px' }}
-                                pad-v8
-                                m-mgn-v8
-                                fg-white
-                                size-6
-                                s-size-8
-                                m-size-10
-                                text-center>
+                        <Container mgn-t8 pad-t4>
+                            <Container text-center size-10 weight-8>
                                 FAQ
                             </Container>
                             <Container>
                                 {
                                     FAQ.map(qa => (
-                                        <Container mgn-t8 key={qa.question}>
-                                            <Container size-5 weight-4 line-6 weight-6>
+                                        <Container mgn-t8 line-8 key={qa.question} style={{ borderLeft: '4px solid #fc0' }} pad-l4>
+                                            <Container size-4 weight-4 line-6 weight-6>
                                                 {qa.question}
                                             </Container>
-                                            <Container size-4 line-6 >
+                                            <Container size-4 >
                                                 {qa.answer}
                                             </Container>
                                         </Container>
